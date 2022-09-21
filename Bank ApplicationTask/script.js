286f92f6b51c04ff18d2cbab3e068e9ad68a7315
Adwait Deshpande <adwaitdeshpande.and@gmail.com>
2022-09-21 12:40:51 +0530

    Changes Done

--- Bank ApplicationTask/script.js
+++ Bank ApplicationTask/script.js
@@ -6,20 +6,19 @@ function sendMoney() {
 
     if (enterAmount > 8000) {
         alert("Insufficient Balance.")
-    } else {
-        var findUserBankAccount = enterName + "BankBalance";
-        var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
-        var myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText) - enterAmount
-        document.getElementById("myAccountBalance").innerText = myAccountBalance
-        document.getElementById(findUserBankAccount).innerHTML = finalAmount;
-        alert(`Successful Transaction !!  
-      $${enterAmount} is sent to ${enterName}@email.com.`)
+        } else {
+            var findUserBankAccount = enterName + "BankBalance";
+            var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerText) + enterAmount;
+            var myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText) - enterAmount
+            document.getElementById("myAccountBalance").innerText = myAccountBalance
+            document.getElementById(findUserBankAccount).innerText = finalAmount;
+            alert(`Successful Transaction !!  $${enterAmount} is sent to ${enterName}@email.com.`)
 
-        // transaction history 
-        var createPTag = document.createElement("li");
-        var textNode = document.createTextNode(`$${enterAmount} is sent to recepient with Email-id ${enterName}@email.com on ${Date()}.`);
-        createPTag.appendChild(textNode);
-        var element = document.getElementById("transaction-history-body");
-        element.insertBefore(createPTag, element.firstChild);
+            // transaction history 
+            var createPTag = document.createElement("li");
+            var textNode = document.createTextNode(`$${enterAmount} is sent to recepient with Email-id ${enterName}@email.com on ${Date()}.`);
+            createPTag.appendChild(textNode);
+            var element = document.getElementById("transaction-history-body");
+            element.insertBefore(createPTag, element.firstChild);
+        }
     }
-}
